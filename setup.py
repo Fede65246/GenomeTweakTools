610f55ff28c07021a33d2216f1cf3ff20e663f06
Fede65246 <75035563+Fede65246@users.noreply.github.com>
2020-12-28 15:45:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'GenomeTweakTools',
+  packages = ['GenomeTweakTools'],  
+  version = '0.1'
+  license='MIT',    
+  description = 'Tools for indirectly manipulate genome sequences or strings',  
+  author = 'Federicvo Vidoli',             
+  author_email = 'federico.vidoli@liceovirgilioroma.com',
+  url = 'https://github.com/Fede65246/GenomeTweakTools',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['STRING MANIPULATION', 'GENOME EDITING'],
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
